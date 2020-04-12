8664c8ba25572edbd8fded46cbd29d2516678410
Vu Anh Duc <vuanhduc2685@gmail.com>
2020-04-12 23:33:46 +0700

    hard code ads

--- src/client.js
+++ src/client.js
@@ -1,12 +1,12 @@
-import * as sapper from '@sapper/app';
-import { googleAnalytics } from './gtm';
-import { googleAdsense } from './adsense'
+import * as sapper from "@sapper/app";
+import { googleAnalytics } from "./gtm";
+// import { googleAdsense } from "./adsense";
 
 sapper.start({
-  target: document.querySelector('#sapper')
+  target: document.querySelector("#sapper"),
 });
 
 if (process.env.GTM_ID) googleAnalytics(process.env.GTM_ID);
-if (process.env.GOOGLE_ADSENSE_ID) {
-  googleAdsense(process.env.GOOGLE_ADSENSE_ID)
-}
+// if (process.env.GOOGLE_ADSENSE_ID) {
+//   googleAdsense(process.env.GOOGLE_ADSENSE_ID)
+// }
