77b87f00618a07016e45cdc8ef5faebd2ed8ee44
Vu Anh Duc <vuanhduc2685@gmail.com>
2020-04-12 23:29:22 +0700

    code format

--- src/adsense.js
+++ src/adsense.js
@@ -1,8 +1,8 @@
 export const googleAdsense = (pubId) => {
-    const headEl = document.getElementsByTagName('head')[0]
-    const newScript = document.createElement('script')
-    newScript.async = true
-    newScript.setAttribute('data-ad-client', pubId)
-    newScript.src =`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js`
-    headEl.appendChild(newScript)
-}
+  const headEl = document.getElementsByTagName("head")[0];
+  const newScript = document.createElement("script");
+  newScript.async = true;
+  newScript.setAttribute("data-ad-client", pubId);
+  newScript.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js`;
+  headEl.appendChild(newScript);
+};
