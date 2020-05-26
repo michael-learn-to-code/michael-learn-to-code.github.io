0f66367ee5bcef47bb7bb02bf7bdd11b397758b9
Michael Vu <vuanhduc2685@gmail.com>
2020-05-26 14:41:26 +0700

    add amazon ads

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script context="module">
-  let showAll = true
+  let showAll = true;
   export function preload({ params, query }) {
     return this.fetch(`blog.json`)
       .then(r => r.json())
@@ -10,19 +10,31 @@
 </script>
 
 <script>
-  import PostThumbnail from '../components/PostThumbnail.svelte'
-  import UnsplashImage from '../components/UnsplashImage.svelte'
+  import PostThumbnail from "../components/PostThumbnail.svelte";
+  import UnsplashImage from "../components/UnsplashImage.svelte";
+  import Ads from "../components/Ads.svelte";
   export let posts;
-
 </script>
 
 <svelte:head>
   <title>#MLTC - Michael Learn To Code</title>
 </svelte:head>
 
-    <UnsplashImage photoId="PQHOmT-vkgA" author="Norbert Kowalczyk" alt="find myself in space" />
-<main class="flex flex-col content-center w-full max-w-full lg:p-4 sm:p-2 md:p-2">
+<UnsplashImage
+  photoId="PQHOmT-vkgA"
+  author="Norbert Kowalczyk"
+  alt="find myself in space" />
+<main
+  class="flex flex-col content-center w-full max-w-full lg:p-4 sm:p-2 md:p-2
+  grid sm:grid-cols-12">
+  <div
+    class="ads-display sm:col-start-1 sm:col-end-3 flex flex-col items-center
+    invisible hidden sm:visible sm:block">
+    <Ads />
+  </div>
+  <div class="content sm:col-start-3 sm:col-end-12">
     {#each posts as post, index}
-      <PostThumbnail post={post} />
+      <PostThumbnail {post} />
     {/each}
+  </div>
 </main>
